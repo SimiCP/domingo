682194a064c4c0f4eeb8a5e7b4b87a5355bcbc4f
brunno marquezini <marquezini.brunno@gmail.com>
2012-11-03 13:27:57 -0600

    seven almost final?

--- program seven/programSeven.java
+++ program seven/programSeven.java
@@ -2,23 +2,26 @@
 // for the Mathematical Association of America, the organization reimburses 
 // each official state delegate according to the round trip mileage. The
 // mileage data is stored in miles.txt (one value per line), each positive  
-// value will be calculated to a new amount(currency) value.
+// value will be calculated to a new amount equal to a currency value.
 // The program will create an output file milesamount.txt with the calculated
-// reimbursement amounts for each mile value processed, the output is displayed
+// reimbursements for each mileage value processed, the output is displayed
 // on the outfile with a header containing the title and two columns (miles) 
 // and (amount) similar to a table format, each column is adjusted to align
-// vertically by decimal points.
+// vertically by decimal points, the numbers are to align with the header.
 // If the processed mileage value is 0 or negative, the program will write 
 // "*****" to the (amount) column aligned with the processed mileage value in the
-// the (miles) column after the table it will print three lines, the first line 
+// the (miles) column. After the table it will print three lines, the first line 
 // with the total of reimbursement values, the second contains the number of 
 // mileage values processed, and the third contains the number of mileage values
 // greater or equal to zero. 
-// Methods used:
-// 	header() -- prints the heading for the table on the output file
-//		output() -- outputs summary information on the output file
-// 	leftpad()  -- adjusts the processed values to be aligned by one decimal point
-//		leftpad2() -- adjusts the processed values to be aligned by two decimal points
+// Methods used:                 //header() and output() inside main method
+//  header() - prints the heading for the table on the output file
+//	 output() - outputs summary information on the output file
+//  leftpad() - adjusts the processed values to be aligned by one decimal point
+//	 leftpad2() - adjusts the processed values to be aligned by two decimal points
+// Brunno Putnam
+// Program #7, CS 1050, Fall 2012, MW
+// Java(JDK 7u9) Platform SE binary, USUS UX31E, windows 7
 
 
 import java.io.*;        	              
@@ -26,37 +29,38 @@ import java.text.*;
 import java.util.Scanner;					  
 
 public class programSeven 					  
-{		// main method
+{	// main method
 	public static void main (String [] Args) throws Exception	
 	{ 
-    	double miles;   	 // declaration of miles variable
-   	double amount;		 // declaration of amount variable
+    	double miles;   	 // holds the miles value from input file
+   	double amount;		 // holds the amount value calculated from mileage
 				
-		 		// reads file
+		// reads file
 		Scanner inFile = new Scanner(new FileReader ("miles.txt"));
-				// creates a new file where the output informtion will be printed on
+		// creates a new file where the output informtion will be printed on
 		PrintWriter outFile = new PrintWriter("milesamount.txt");
-				// call for the header method, prints the header in the output
+		// call for the header method, prints the header in the output
 		header( outFile );
 		
 		double totalAmount = 0; 	// declares and sets the variable totalAmount to zero
 		double totalMiles = 0;		// declares and sets the variable totalMiles to zero
 	   int n = inFile.nextInt();  // declares n and processes the values from the input file,
 											// n is the amount of values processed
+											
 		int counter = 0;				// declares and sets the variable (counter) to zero
 											// (counter) holds the number of positive values processed
 											
       for (int i = 0; i < n; i++)      // number of values to process (n)
       {    	
 			amount = 0;  
-				// sets the variable (miles) to acquire the next value from input data file							
+			// sets the variable (miles) to acquire the next value from input data file							
        	miles = inFile.nextDouble();
 			
-				// if statment to calculate all mileage values from input file above zero
-				// each value will pass through the if and else if statements and will be calculated
-				// into a new (amount) value to correspond to the according mileage value
+			// if statments are to calculate all mileage values from input file above zero
+			// each value will pass through the if and else if statements and will be 
+			// calculated into a new (amount) value to correspond to the according mileage
         	if (miles > 0)						
-        	{  	// (counter) adds one to variable for each positive mileage from input file
+        	{  // (counter) adds one to variable for each positive mileage from input file
 			   ++counter; 			   
           	if(miles < 500)			   
               amount = 0.15 * miles;   
@@ -71,83 +75,83 @@ public class programSeven
             else 
               amount  = 285 + 0.05 *(miles - 3000);
 				  
-					// increases the totalMiles by the positive miles values found in the file
+				// increases the totalMiles by the positive miles values found in the file
 				totalMiles += miles;							 
 				  
-					// sets the proper spacing on the outfile, leftpad and leftpad2 miles
-					// align miles and amount values by one or two decimal points
+				// sets the proper spacing on the outfile, leftpad and leftpad2 miles
+				// align miles and amount values by one or two decimal points
 				outFile.println(leftpad( miles, 10 ) + leftpad2( amount, 10 )); 
 		  }
         else 
         { 	
 		  		if (miles == 0) 					
-					counter++;		// adds one more to count for values equal to zero
+					counter++;		// adds one more to count for a value equal to zero
 				amount = 0; 						
-					// print the mileage values with a special decimal notation
+				// print the mileage values with a special decimal notation
             outFile.println(leftpad( miles, 10 ) + "     *****" );
 		  }
 		  totalAmount += amount;				// total amounts
 	   }
-			// write file footer
+		// write file footer
 		output( outFile, totalAmount, counter, n);
 	}
-//***********************************************************************************************
-		// method header will print the header format to the output file(outFile)
+//********************************************************************************************
+	// method header will print the header format to the output file(outFile)
    public static void header(PrintWriter outFile)
-   {	   // title 
+   {	// title 
     	outFile.println("           MMA");	
 			// column header 
     	outFile.println("     miles    amount");
 	}
-//***********************************************************************************************
-		// print footer
-		// this method outputs the total reimbursment amount (totalAmount)
-		// outputs the number of mileage values processed (n)
-		// outputs the number of values >= 0 (counter)
+//********************************************************************************************
+	// print footer
+	// this method outputs the total reimbursment amount (totalAmount)
+	// outputs the number of mileage values processed (n)
+   // outputs the number of values >= 0 (counter)
 	public static void output(PrintWriter outFile, double totalAmount, int counter, int n)
-   {  	// line break before footer
+   {  // line break before footer
 		outFile.println();						
-			// the total of amounts
+		// the total of amounts using special formatting from static method leftpad2
 		outFile.println("total amount of " + leftpad2( totalAmount, 0));
-			// the count of entries to be read from the input file
+		// the count of entries to be read from the input file
 		outFile.println("number of mileage values procesed is " + n);
-			// prints the count of entries without negatives miles
+		// prints the count of entries without negatives miles
 		outFile.println("number of mileage values is " + counter);
-			// writes output then closes the file
+		// writes output then closes the file
 		outFile.close(); 
   }
-//************************************************************************************************
-	  // decimal format for amount
+//*********************************************************************************************
+  // decimal format for amount
   public static String leftpad2(double amount, int width)
   {	
   		String s;         // string that is returned
    	int m;            // lenght of string (s)
   
    	NumberFormat money = NumberFormat.getCurrencyInstance();
-  			// convert miles to a string with two decimal places
+  		// convert miles to a string with two decimal places
    	s = money.format(amount);
-  			// determine the lenght of string (s)
+  		// determine the lenght of string (s)
    	m = s.length();
-  			// pad s by spaces on the left so that the resulting lenght of s is width
+  		// pad s by spaces on the left so that the resulting lenght of s is width
    	for(int i = 0; i < width-m; i++)
   		{
 			s = " " + s;   // one space between the " "
   		}
   		return s;
   }
-// ***********************************************************************************************
-		// decimal format for miles
+// ********************************************************************************************
+  // decimal format for miles
   public static String leftpad(double miles, int width)
   {	
 		String s;         // string that is returned
   		int m;            // lenght of string (s)
   
   		DecimalFormat fmt = new DecimalFormat("0.0");
-  			// convert miles to a string with one decimal place
+  		// convert miles to a string with one decimal place
   		s = fmt.format(miles);
-  			// determine the lenght of string (s)
+  		// determine the lenght of string (s)
   		m = s.length();
-  			// pad s by spaces on the left so that the resulting lenght of s is width
+  		// pad s by spaces on the left so that the resulting lenght of s is width
   		for (int i = 0; i < width - m; i++)
   		{
   			s = " " + s;   // one space between the " "
